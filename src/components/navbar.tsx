0484b417192fea9f083b6f011de71c53cd838e86
Songwaad H <songw44d@gmail.com>
2025-10-20 21:01:10 +0700

    refactor: clean up Navbar component code and improve formatting

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,38 @@
 import { Link } from "react-router";
 import { Button } from "./ui/button";
 import { useAuth } from "@/context/AuthContext";
-import { toast } from "sonner";
 
 export function Navbar() {
-    const { user, logout } = useAuth();
+  const { user, logout } = useAuth();
 
-    const handleLogout = async () => {
-        await logout();
-        toast.success("Logged out successfully");
-        window.location.href = "/";
-    };
+  const handleLogout = async () => {
+    await logout();
+    window.location.href = "/";
+  };
 
-    return (
-        <nav className="fixed left-1/2 px-4 rounded-2xl -translate-x-1/2 top-4 w-5xl flex justify-between p-2 bg-gray-200">
-            <div className="flex items-center space-x-4">
-                <p className="font-bold text-lg">KornKK</p>
-                <Link to="/"><Button variant="ghost">Home</Button></Link>
-            </div>
+  return (
+    <nav className="fixed left-1/2 px-4 rounded-2xl -translate-x-1/2 top-4 w-5xl flex justify-between p-2 bg-gray-200">
+      <div className="flex items-center space-x-4">
+        <p className="font-bold text-lg">KornKK</p>
+        <Link to="/">
+          <Button variant="ghost">Home</Button>
+        </Link>
+      </div>
 
-            <div className="flex items-center space-x-2">
-                {user ? (
-                    <>
-                        <p className="text-sm">Hello, {user.username}</p>
-                        <Button className="cursor-pointer" onClick={handleLogout}>Logout</Button>
-                    </>
-                ) : (
-                    <Link to="/login"><Button className="cursor-pointer">Login</Button></Link>
-                )}
-            </div>
-        </nav>
-    );
+      <div className="flex items-center space-x-2">
+        {user ? (
+          <>
+            <p className="text-sm">Hello, {user.username}</p>
+            <Button className="cursor-pointer" onClick={handleLogout}>
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Link to="/login">
+            <Button className="cursor-pointer">Login</Button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
 }
